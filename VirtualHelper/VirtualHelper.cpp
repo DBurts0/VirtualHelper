3cc3d051fd1595964c8b0d597fb5e75b8e875f10
DBurts0 <devinburts0@gmail.com>
2022-06-02 11:51:56 -0500

    1

--- VirtualHelper/VirtualHelper.cpp
+++ VirtualHelper/VirtualHelper.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+// Variables for storing the user's inputs
 string us;
 string subject;
 string info;
@@ -17,6 +18,7 @@ void AIHelper()
 QUESTION:
     cout << "Read or Write mode?\n";
     cin >> userIn;
+    // display everything the user has told the ai helper
     if (userIn == "read")
     {
         iFile.open("myText");
@@ -25,43 +27,51 @@ QUESTION:
         {
             cout << line << endl;
         }
+        // close the file
         iFile.close();
     }
+    // Store info for the ai to remember
     else if (userIn == "write")
     {
-        iFile.open("myText", ios::out | ios::in | ios::app);
+        iFile.open("myText", ios::out| ios :: in|ios::app);
+        // ask for the user's name
         cout << "What is your name?\n";
         cin >> us;
         cout << "Hello " << us << endl;
+        // choose what type of information the ai needs to store
         HELP:
-        cout << "What do you want me to remember? Type help for options\n";
+        cout << "What do you want me to remember?\n";
+        cout << "Available commands: age, item, shopping, appointment, other.\n ";
         cin >> subject;
+        // Remember a certain person's age
         if (subject == "age")
         {
             subject.append(" ");
             cout << "Who's age do you want me to remember?\n";
             string age;
             cin >> age;
-            cout << "\n How old is" << age << "?\n";
+            cout << "\n How old is " << age << "?\n";
             cin >> info;
             cout << "\n I will remember that " << age << " is currently " << info << " years old.\n";
             subject.append(age);
             subject.append(" ");
             subject.append(info);
         }
+        // remember where an item was placed
         else if (subject == "item")
         {
             subject.append(" ");
             cout << "What item do you want me to remember?\n";
             string item;
             cin >> item;
-            cout << "\n Where is the " << item << "placed?\n";
+            cout << "\n Where is the " << item << " placed?\n";
             cin >> info;
-            cout << "\n I will remember " << item << " is currently " << info << endl;
+            cout << "\n I will remember " << item << " is currently place in/on the " << info << endl;
             subject.append(item);
             subject.append(" ");
             subject.append(info);
         }
+        // remember where an item was placed
         else if (subject == "appointment")
         {
             subject.append(" ");
@@ -70,11 +80,12 @@ QUESTION:
             cin >> appointment;
             cout << "\n What time does the appointment start?\n";
             cin >> info;
-            cout << "I will remember that you have an appointment scheduled for " << appointment << "at " << info << endl;
+            cout << "I will remember that you have an appointment scheduled for " << appointment << " at " << info << endl;
             subject.append(appointment);
             subject.append(" ");
             subject.append(info);
         }
+        // remember an item the user needs to buy
         else if (subject == "shopping")
         {
             subject.append(" ");
@@ -82,7 +93,10 @@ QUESTION:
             cin >> info;
             cout << "\n I will remember that you need to buy " << info << endl;
             subject.append(info);
+
+            subject.append("\n");
         }
+        // remember another piece of information
         else if (subject == "other")
         {
             subject.append(" ");
@@ -96,11 +110,13 @@ QUESTION:
             subject.append(" ");
             subject.append(info);
         }
-        else if (subject == "help")
+        // if the user misspells, immediatley give them another chance to issue a command
+        else
         {
-            cout << "Available commands: age, item, shopping, appointment, other.\n ";
+            cout << "Invalid response\n";
             goto HELP;
         }
+        // store the information and close the file
         subject.append("\n");
         iFile << subject;
 
@@ -117,11 +133,13 @@ int main()
     while (userIn2 != "no")
     {
         AIHelper();
+        CONTINUE:
         cout << "Would you like to continue?\n";
         cin >> userIn2;
-        while (userIn2 != "yes" && userIn2 != "no")
+        if (userIn2 != "yes" && userIn2 != "no")
         {
             cout << "Invalid response. Type yes or no\n";
+            goto CONTINUE;
         }
     }
 }
